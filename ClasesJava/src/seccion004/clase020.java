1db1f16445ec4c87a0283785d84929efdbd7d385
Miguel <Miguel@ALIENWARE>
2018-04-11 17:01:33 -0300

    Update clases

---
+++ ClasesJava/src/seccion004/clase020.java
@@ -0,0 +1,68 @@
+package seccion004;
+
+public class clase020 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// variables boolean, declaracion
+		boolean bool1;
+		// inicalizacion
+		bool1 = true;
+		boolean bool2 = false;
+		System.out.println("Valor bool1: " + bool1);
+		System.out.println("Valor bool2: " + bool2);
+		System.out.println("");
+		
+		//Variables Byte
+		byte b1 = 10;
+		//Literal en hexadecimal
+		byte b2 = 0xa;
+		System.out.println("Valor byte1: " + b1);
+		System.out.println("Valor byte2: " + b2);
+		System.out.println("");
+		
+		//Variables short
+		short s1 = 2;
+		System.out.println("Valor short1: " + s1);
+		System.out.println("");
+		
+		//Variable char, declaracion e inicializacion de un a sola linea
+		//La primera declaracion es en UNICODE
+		char ch1 = 65, ch2 ='A';
+		System.out.println("Valor char1: " + ch1);
+		System.out.println("Valor char2: " + ch2);
+		System.out.println("");
+		
+		//Variable enteras
+		int decimal = 100;
+		int octal = 0144; //Valor se inicia con 0
+		int hexa = 0x64; //Valor se inicia con 0x
+		System.out.println("Valor decimal: " + decimal);
+		System.out.println("Valor bool1: " + octal);
+		System.out.println("Valor bool1: " + hexa);
+		System.out.println("");
+		
+		//Variables long
+		long long1 = 10, long2 = 20L;
+		System.out.println("Valor long1: " + long1);
+		System.out.println("Valor long2: " + long2);
+		System.out.println("");
+		
+		//Variables Floeat
+		float f1 = 15, f2 = 22.3F;
+		System.out.println("Valor float1: " + f1);
+		System.out.println("Valor float2: " + f2);
+		System.out.println("");
+		
+		//Variables double
+		double d1 = 11.0, d2 = 30.15D;
+		System.out.println("Valor double1: " + d1);
+		System.out.println("Valor double2: " + d2);
+		System.out.println("");
+		
+		
+		
+		
+	}
+
+}
