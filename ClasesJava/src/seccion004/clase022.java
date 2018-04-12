5a01cf543129deb030e90fb2b117a21d13128946
Miguel <Miguel@ALIENWARE>
2018-04-12 10:58:35 -0300

    clase 22

---
+++ ClasesJava/src/seccion004/clase022.java
@@ -0,0 +1,18 @@
+package seccion004;
+
+public class clase022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Miguel";
+		String apellido = "Peralta";
+		System.out.println("Concatenacion: " + nombre + apellido);
+		System.out.println("Nueva Linea: \n" + nombre + " " + apellido);
+		System.out.println("Tabulador: \t" + nombre + " " + apellido);
+		System.out.println("Retroceso:\b" + nombre + " " + apellido);
+		System.out.println("Retorno de carro: \r" + nombre + " " + apellido);
+		System.out.println("Comilla simple: \'" + nombre + " " + apellido + "\'");
+		System.out.println("Comilla doble: \"" + nombre + " " + apellido + "\"");
+	}
+
+}
